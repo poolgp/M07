ddb5552138836675bbc4368fb969f9d4ef491df0
poolgp <145465364+poolgp@users.noreply.github.com>
2023-12-13 11:47:30 +0100

    13/12/2023

--- Col·leccions/land/land.js
+++ Col·leccions/land/land.js
@@ -1,11 +1,11 @@
-document.getElementById("containerCancion").style.display = "block";
-document.getElementById("containerCantante").style.display = "block";
-document.getElementById("containerCanc").style.display = "none";
-document.getElementById("containerCant").style.display = "none";
+// document.getElementById("containerCancion").style.display = "block";
+// document.getElementById("containerCantante").style.display = "block";
+// document.getElementById("containerCanc").style.display = "none";
+// document.getElementById("containerCant").style.display = "none";
 
-function toggleVisibility() {
-    document.getElementById("containerCancion").style.display = "none";
-    document.getElementById("containerCantante").style.display = "none";
-    document.getElementById("containerCanc").style.display = "block";
-    document.getElementById("containerCant").style.display = "block";
-}
+// function toggleVisibility() {
+//     document.getElementById("containerCancion").style.display = "none";
+//     document.getElementById("containerCantante").style.display = "none";
+//     document.getElementById("containerCanc").style.display = "block";
+//     document.getElementById("containerCant").style.display = "block";
+// }
