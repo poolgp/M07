e4b6404527120440b9fb7a1cdd41812d21084e6b
poolgp <145465364+poolgp@users.noreply.github.com>
2023-12-14 12:29:13 +0100

    14/12/2023

---
+++ Col·leccions/Añadir/añadirCancion.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtiene el elemento del checkbox y el contenedor del segundo artista
+    var checkbox = document.getElementById("flexSwitchCheckDefault");
+    var artista2Container = document.getElementById("artista2Container");
+
+    // Añade un evento de cambio al checkbox
+    checkbox.addEventListener("change", function () {
+        // Si el checkbox está marcado, muestra el campo del segundo artista; de lo contrario, ocúltalo
+        artista2Container.style.display = checkbox.checked ? "block" : "none";
+    });
+});
