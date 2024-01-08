ed28760ec1ac278c8e6850294165e31046c04ac4
poolgp <145465364+poolgp@users.noreply.github.com>
2024-01-08 15:51:16 +0100

    08/01/2024

---
+++ Col·leccions/cantantes/cantantes.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cantante.com</title>
+
+    <script src="https://kit.fontawesome.com/c7334dda94.js" crossorigin="anonymous"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+
+    <link rel="stylesheet" href="cantantes.css">
+</head>
+
+<body>
+    <div class="containerTitulo">
+        <div class="title-container">
+            <h1>Cantante.com / Cantantes</h1>
+        </div>
+        <div class="button-container">
+            <a href="../canciones/canciones.php">
+                <button class="btn btn-outline-success">Ir a Canciones</button>
+            </a>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <ul class="navbar-nav">
+                        <li>
+                            <a href="../Añadir/añadir.php">
+                                <span>Añadir</span>
+                                <i class="fa-solid fa-plus"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span>Modificar</span>
+                                <i class="fa-solid fa-pen-to-square"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span>Eliminar</span>
+                                <i class="fa-solid fa-trash-can"></i>
+                            </a>
+                        </li>
+                        <li class="dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span>Ordenar</span>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <a class="dropdown-item" href="#">Título</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Artista</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Álbum</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <div class="container-fluid">
+                                <form class="d-flex" role="search">
+                                    <input class="form-control me-2" type="search" placeholder="Search"
+                                        aria-label="Search">
+                                    <button class="btn btn-outline-success" type="submit">Search</button>
+                                </form>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="containerCartas">
+        <div>
+
+        </div>
+    </div>
+
+
+</body>
+
+</html>
+<!-- <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div> -->
