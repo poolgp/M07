4d409ad34dd13d90cd81f6b03493d6798fa6d09a
poolgp <145465364+poolgp@users.noreply.github.com>
2024-02-13 11:20:55 +0100

    12/02/2024

---
+++ colecciones2/home/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cantante.com</title>
+
+    <script src="https://kit.fontawesome.com/c7334dda94.js" crossorigin="anonymous"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+
+    <style>
+    body {
+        background-color: #191414;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+    }
+
+    header {
+        width: 100%;
+        text-align: center;
+    }
+
+    header img {
+        max-width: 100%;
+        height: auto;
+        margin: 25px;
+    }
+
+    .card {
+        width: 18rem;
+    }
+
+    .card-container {
+        text-align: center;
+        margin-top: 25px; /* Ajusta el margen superior según sea necesario */
+    }
+    </style>
+    <script src="index.js"></script>
+</head>
+
+<body>
+    <header>
+        <div class="LogoTitulo">
+            <img class="LogoTituloimg" src="LogoTitulo.png" />
+        </div>
+    </header>
+
+    <div class="card-container">
+        <div class="card">
+            <div class="card-header">
+                Featured
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
