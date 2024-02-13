eaf14fb04b66021c4b8a060da801fd4b000b61f6
poolgp <145465364+poolgp@users.noreply.github.com>
2024-02-13 13:30:33 +0100

    13/02/2024

--- colecciones2/home/index.php
+++ colecciones2/home/index.php
@@ -12,62 +12,91 @@
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+        </script>
 
     <style>
-    body {
-        background-color: #191414;
-        margin: 0;
-        padding: 0;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        min-height: 100vh;
-    }
+        body {
+            background-color: #191414;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+        }
 
-    header {
-        width: 100%;
-        text-align: center;
-    }
+        header {
+            width: 100%;
+            text-align: center;
+        }
 
-    header img {
-        max-width: 100%;
-        height: auto;
-        margin: 25px;
-    }
+        .botones {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin-top: 20px;
+            width: 100%;
+            max-width: 800px;
+        }
 
-    .card {
-        width: 18rem;
-    }
+        .boton {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            background-color: #1ED760;
+            border-radius: 30px;
+            width: 100%;
+            height: 100px;
+            margin: 10px;
+            padding: 10px;
+            cursor: pointer;
+        }
 
-    .card-container {
-        text-align: center;
-        margin-top: 25px; /* Ajusta el margen superior según sea necesario */
-    }
+        .boton h3 {
+            color: #fff;
+            margin: 0 10px;
+        }
+
+        .boton a {
+            display: block;
+            width: 100%;
+            height: 100%;
+        }
+
+        .boton .icono {
+            margin-right: 10px;
+            color: #fff;
+        }
     </style>
     <script src="index.js"></script>
 </head>
 
-<body>
-    <header>
-        <div class="LogoTitulo">
-            <img class="LogoTituloimg" src="LogoTitulo.png" />
-        </div>
-    </header>
-
-    <div class="card-container">
-        <div class="card">
-            <div class="card-header">
-                Featured
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Vestibulum at eros</li>
-            </ul>
-        </div>
+<header>
+    <div class="LogoTitulo">
+        <img class="LogoTituloimg" src="../LogoTitulo.png" />
     </div>
+</header>
+
+<body>
+    <section class="botones">
+        <a href="../añadirCantante/añadirCantante.php" class="text-decoration-none text-white boton">
+            <i class="fa-solid fa-plus icono"></i>
+            <h3>Añadir Cantante</h3>
+        </a>
+
+        <a href="../añadirCancion/añadirCancion.php" class="text-decoration-none text-white boton">
+            <i class="fa-solid fa-plus icono"></i>
+            <h3>Añadir Cancion</h3>
+        </a>
+
+        <a href="../lista/lista.php" class="text-decoration-none text-white boton">
+            <i class="fa-solid fa-list-ul icono"></i>
+            <h3>Mostrar Lista</h3>
+        </a>
+    </section>
 </body>
 
 </html>
